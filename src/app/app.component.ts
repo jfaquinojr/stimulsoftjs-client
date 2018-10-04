d7a25ded2b14e5b55c859872b9797ac62383e316
Jojo Aquino <jfaquinojr@gmail.com>
2018-10-04 09:10:59 +0800

    show designer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stimulsoft } from 'stimulsoft-reports-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stimulsoft-client';
+
+  ngOnInit() {
+    const designer = new Stimulsoft.Designer.StiDesigner();
+    designer.renderHtml('designer');
+  }
+
 }
